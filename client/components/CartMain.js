18663b28b119207ef14d2ae8a0ed726a4a9b48e9
Rijin Lee <rijinlee@gmail.com>
2019-09-18 17:24:57 -0400

    changes to the CartMain component

--- client/components/CartMain.js
+++ client/components/CartMain.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-import {fetchCart} from '../store/order'
 import SingleCartItem from './SingleCartItem'
 import CartSummary from './CartSummary'
 
 class DisconnectedCartMain extends React.Component {
   componentDidMount() {
-    const orderId = this.props.match.params.orderId
-    this.props.fetchCart(orderId)
+    // const orderId = this.props.match.params.orderId
+    // this.props.fetchCart(orderId)
   }
 
   render() {
-    const allCartItems = this.props.allCartItems
-
+    const {allCartItems} = this.props
+    console.log(allCartItems)
     return (
       <div>
         {allCartItems.map(cartItem => {
@@ -23,7 +22,7 @@ class DisconnectedCartMain extends React.Component {
             </div>
           )
         })}
-        <CartSummary />
+        <CartSummary /> //need to pass some cart order info as props
       </div>
     )
   }
@@ -31,18 +30,16 @@ class DisconnectedCartMain extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    allCartItems: state.cart
+    allCartItems: state.order.cart
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchCart: orderId => dispatch(fetchCart(orderId))
-  }
-}
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchCart: orderId => dispatch(fetchCart(orderId))
+//   }
+// }
 
-const CartMain = connect(mapStateToProps, mapDispatchToProps)(
-  DisconnectedCartMain
-)
+const CartMain = connect(mapStateToProps)(DisconnectedCartMain)
 
 export default CartMain
