0586ebbd27772b7a3a2f88bf41ac2444d264adec
Helen Qiong Zhang <helenqiongzhang@gmail.com>
2019-09-19 12:03:06 -0400

    added componentdidmount in cartSummary component

--- client/components/CartMain.js
+++ client/components/CartMain.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchProducts} from '../store/product'
 
 import SingleCartItem from './SingleCartItem'
 import CartSummary from './CartSummary'
-import {removingCartItem} from '../store/order'
+import {removingCartItem, fetchCart} from '../store/order'
 
 class DisconnectedCartMain extends React.Component {
+  componentDidMount() {
+    this.props.getProducts()
+    this.props.fetchCart(this.props.user.id)
+  }
   render() {
-    console.log(this.props)
+    console.log('🤗🤗🤗🤗🤗🤗', this.props)
     const {allCartItems} = this.props
     // const {removingCartItem} = this.props
     return (
@@ -30,12 +35,16 @@ class DisconnectedCartMain extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    allCartItems: state.order.cart
+    allCartItems: state.order.cart,
+    user: state.user,
+    products: state.product.all
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
+    getProducts: () => dispatch(fetchProducts()),
+    fetchCart: userId => dispatch(fetchCart(userId)),
     removingCartItem: productId => dispatch(removingCartItem(productId))
   }
 }
