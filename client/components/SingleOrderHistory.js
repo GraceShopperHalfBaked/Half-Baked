d27e5234f2bba00b80ad08363888036c65b5f5bc
Rijin Lee <rijinlee@gmail.com>
2019-09-24 17:34:10 -0400

    added order history

--- client/components/SingleOrderHistory.js
+++ client/components/SingleOrderHistory.js
@@ -2,13 +2,26 @@ import React from 'react'
 
 const SingleOrderHistory = props => {
   const {oneOrder} = props
+
   return (
     <div>
-      <h3>{oneOrder.createdAt}</h3>
+      <h3>{oneOrder.createdAt.slice(0, 10)}</h3>
       <div>
-        <div>Order number: {oneOrder.id}</div>
-        <div>Order status: {oneOrder.cartStatus}</div>
-        <div>Order total: {(oneOrder.totalOrderPrice / 100).toFixed(2)}</div>
+        <div>Order Number: {oneOrder.id}</div>
+        <div>Order Status: {oneOrder.cartStatus}</div>
+        <div>Order Total: {(oneOrder.totalOrderPrice / 100).toFixed(2)}</div>
+      </div>
+      <div>
+        Order Details:
+        {oneOrder.products.map(itemInOrder => {
+          return (
+            <div key={itemInOrder.id}>
+              <div>{itemInOrder.name}</div>
+              <div>{(itemInOrder.currentPrice / 100).toFixed(2)}</div>
+              <img src={itemInOrder.imageUrl} />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
